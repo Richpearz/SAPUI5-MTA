42ebd4b36d583291f8877526f3f57a8c20dfcb4c
Richpearz <rperez@saljamex.com>
2023-02-25 03:15:41 +0000

    Commit V_1.0.2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,23 @@
+// @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("richpearz.projectfioriexp.controller.App", {
             onInit: function () {
+                let oJSONModel = new sap.ui.model.json.JSONModel(),
+                    oView = this.getView(),
+                    oResourceBundle = oView.getModel("i18n").getResourceBundle();
 
+                oJSONModel.loadData("../localService/mockdata/Employees.json");
+                oView.setModel(oJSONModel, "richpearzEmployee");
             },
 
             onValidate : function(oEvent) {
@@ -20,6 +28,7 @@ sap.ui.define([
 
                 //console.log(oInput);
 
+                /*
                 if (sValue.length === oInput.getMaxLength()) {
                     oInput.setDescription("Ok");
 
@@ -32,7 +41,65 @@ sap.ui.define([
                     this.getView().byId("labelCountry").setVisible(false);
                     this.getView().byId("selectCountry").setVisible(false);
 
+                } 
+                */               
+            },
+
+            onFilter : function () {
+                var oJSONModel = this.getView().getModel("richpearzEmployee").getData(),
+                    aFilters = [];
+
+                /*
+                if (oJSONModel.employeeId  !== ""){
+                    aFilters.push(new Filter("EmployeeID", FilterOperator.Contains, oJSONModel.employeeId));
                 }
+                */
+
+                if (oJSONModel.employeeId  !== ""){
+                    aFilters.push(new Filter({
+                        filters:[
+                            new Filter("EmployeeID", FilterOperator.Contains, oJSONModel.employeeId),
+                            new Filter("FirstName", FilterOperator.Contains, oJSONModel.employeeId)
+                        ],
+                        and: false
+                    }));
+                }
+
+                if (oJSONModel.countryKey !== ""){
+                    aFilters.push(new Filter("Country", FilterOperator.EQ, oJSONModel.countryKey));
+                }
+
+                var oList = this.getView().byId("tableEmployee");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters);
+
+                var iAmount = oBinding.aIndices.length;
+                this.getView().byId("tableEmployee").setHeaderText("Employees: ("+ iAmount +")");
+
+            },
+
+            onClearFilter () {
+                let oModel = this.getView().getModel("richpearzEmployee");
+                
+                oModel.setProperty("richpearzEmployee>/employeeId");
+                oModel.setProperty("richpearzEmployee>/countryKey");
+
+                let oTable = this.getView().byId("tableEmployee"),                
+                oBinding = oTable.getBinding("items");
+                oBinding.filter([]);
+
+                this.byId("inputEmployee").setValue("");
+                this.byId("selectCountry").setValue("");
+                
+                this.getView().byId("tableEmployee").setHeaderText("Employees: (" + oModel.oData.Amount + ")");
+            },
+
+            onMessage (oEvent) {
+                let oBindingContext = oEvent.getSource().getBindingContext("richpearzEmployee");
+
+                console.log(oBindingContext.getObject()); 
+
+                new sap.m.MessageToast.show(oBindingContext.getObject().PostalCode);
             }
         });
     });
