9b4baea79c83a38649ec44828aa449d2139a22ed
Richpearz <rperez@saljamex.com>
2023-02-25 01:21:01 +0000

    Commit V_1.0.1

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,29 @@ sap.ui.define([
         return Controller.extend("richpearz.projectfioriexp.controller.App", {
             onInit: function () {
 
+            },
+
+            onValidate : function(oEvent) {
+                //console.log(oEvent.getParameters());
+
+                let sValue = oEvent.getParameter("newValue"),
+                    oInput = oEvent.getSource(); // = this.byId("inputEmployee");
+
+                //console.log(oInput);
+
+                if (sValue.length === oInput.getMaxLength()) {
+                    oInput.setDescription("Ok");
+
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("selectCountry").setVisible(true);
+
+                } else {
+                    oInput.setDescription("Not Ok");
+
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("selectCountry").setVisible(false);
+
+                }
             }
         });
     });
